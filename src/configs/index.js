1007a7b19cec4681e33f1126091bad3fe6c6d3dc
anon-kae <anon.ksl42@gmail.com>
2022-12-21 02:06:21 +0700

    feat: update validate env

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,8 +2,15 @@ const joi = require('joi');
 
 const environmentSchema = {
   NODE_ENV: joi.string().valid('production', 'development', 'test').required(),
+  HOST: joi.string().required(),
   PORT: joi.number().default(8000),
   CORS_WHITELIST_ORIGINS: joi.string().required(),
+  JWT_SECRET: joi.string().required(),
+
+  /* Logging */
+  LOG_LEVEL: joi.string().required(),
+  LOG_DB_QUERY: joi.boolean().required(),
+  LOG_STACKDRIVER_CREDENTIALS: joi.string().optional().allow(''),
 
   /* Database PG */
   DB_HOSTNAME: joi.string().required(),
@@ -25,7 +32,15 @@ const environment = value;
 
 module.exports = {
   env: environment.NODE_ENV,
+  host: environment.HOST,
   port: environment.PORT,
+  logLevel: environment.LOG_LEVEL,
+  logQuery: environment.LOG_DB_QUERY,
+  logStackDriverCredentials: environment.LOG_STACKDRIVER_CREDENTIALS,
+  whitelistOrigins: environment.CORS_WHITELIST_ORIGINS,
+  jwt: {
+    secret: environment.JWT_SECRET
+  },
   database: {
     databaseName: environment.DB_NAME,
     hostname: environment.DB_HOSTNAME,
